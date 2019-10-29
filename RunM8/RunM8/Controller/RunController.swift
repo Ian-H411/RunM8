ba9fcde4d569262b843700eb6176c17a7fd39b62
Ian_H411 <ian.h411@gmail.com>
2019-10-29 13:30:45 -0600

    Fixes a mistake with cocoapod dependencies and adds the RunController file

---
+++ RunM8/RunM8/Controller/RunController.swift
@@ -0,0 +1,20 @@
+//
+//  RunController.swift
+//  RunM8
+//
+//  Created by Ian Hall on 10/29/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+class RunController {
+    
+    static let shared = RunController()
+     
+    var selectedUser: User?
+    
+    
+    
+}
