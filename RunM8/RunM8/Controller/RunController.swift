ac73abb6c5531509060f6830cbb916aeb8dfe1e6
Ian_H411 <ian.h411@gmail.com>
2019-10-30 06:10:04 -0600

    adds some error handling and generates the user based on the data we recieve back from the server

--- RunM8/RunM8/Controller/RunController.swift
+++ RunM8/RunM8/Controller/RunController.swift
@@ -14,7 +14,7 @@ class RunController {
     
     
     var selectedUser: User?
-    
+    //MARK: - HELPER FUNCTIONS
     func retrieveValueFromPlist(key: String, plistName: String) -> String {
         guard let filepath = Bundle.main.path(forResource: plistName, ofType: "plist") else { return "error" }
         let propertyList = NSDictionary.init(contentsOfFile: filepath)
@@ -22,17 +22,17 @@ class RunController {
         return value
     }
 
-    
+    //MARK: - CREATE
     func createUser(name:String, completion: @escaping (Bool) -> Void ) {
         let user = User(name: name )
-        selectedUser = user
         let jsonUser = ImportUser(userName: user.userName, id: user.userID, runIDs: user.runIDs, friendIDs: user.friendIDs)
-        guard let baseURL = URL(string: "https://runm8-a2d91.firebaseio.com/") else {return}
+        guard var baseURL = URL(string: "https://runm8-a2d91.firebaseio.com/") else {completion(false);print("could not create url");return}
+        baseURL.appendPathComponent("users")
         let apikey = retrieveValueFromPlist(key: "RunM8Key", plistName: "APIKeys")
         let queryItem = URLQueryItem(name: "key", value: apikey)
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
         urlComponents?.queryItems = [queryItem]
-        guard var url = urlComponents?.url else {return}
+        guard var url = urlComponents?.url else {completion(false);print("url could not be pulled from urlcomponenets");return}
         url.appendPathExtension("json")
         print(url)
         var request = URLRequest(url: url)
@@ -44,22 +44,42 @@ class RunController {
         } catch {
             print("there was an error in \(#function) :\(error) : \(error.localizedDescription)")
         }
-        guard let userDataUn = userData else {return}
+        guard let userDataUn = userData else {completion(false); print("UserData was nil");return}
         request.httpBody = userDataUn
         request.httpMethod = "POST"
         URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("there was an error in \(#function) :\(error) : \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            if let response = response {
+                print(response)
+            }
+            guard let data = data else {completion(false);print("data was found nil");return}
+            do {
+                let decoder = JSONDecoder()
+                let person = try decoder.decode(ImportUser.self, from: data)
+                guard let newUser = User(user: person) else {return}
+                self.selectedUser = newUser
+            } catch {
+                print("there was an error in \(#function) :\(error) : \(error.localizedDescription)")
+                completion(false)
+                return
+            }
             
-        }
+        }.resume()
     }
     
+    //MARK: - READ
     func retrieveUser() {
         
     }
-    
+    //MARK: - UPDATE
     func updateUser() {
         
     }
-    
+    //MARK: - DELTETE
     func deleteUser() {
         
     }
---
+++ RunM8/RunM8/Views/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RunM8
+//
+//  Created by Ian Hall on 10/30/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
