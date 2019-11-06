6aeaac107a9afcca2ba9a4510708038767ef0536
Ian_H411 <ian.h411@gmail.com>
2019-11-06 11:15:14 -0700

    adds a function that takes in a run and converts it to data and prepares the URL

--- RunM8/RunM8/Controller/RunController.swift
+++ RunM8/RunM8/Controller/RunController.swift
@@ -12,21 +12,59 @@ class RunController {
     
     let shared = RunController()
     
+    //MARK: - Helper Functions
+    func retrieveValueFromPlist(key: String, plistName: String) -> String {
+        guard let filepath = Bundle.main.path(forResource: plistName, ofType: "plist") else { return "error" }
+        let propertyList = NSDictionary.init(contentsOfFile: filepath)
+        guard let value = propertyList?.value(forKey: key) as? String else { return "error" }
+        return value
+    }
+    
     //MARK: - ADD A RUN
     func addARun(name: String, route: [CLLocation], time: Int, completion: @escaping (Bool) -> Void ) {
         guard let user = UserController.shared.selectedUser else {return}
-        var startingElevation: Int = Int(route[0].altitude)
+        let startingElevation: Int = Int(route[0].altitude)
         var highestElevation: Int = 0
-        var elevationGained: Int = 0
-        let clPoints = [[Double]]()
+        var clPoints = [[Double]]()
         for point in route {
             //set the highest elevation if current point is higher
             if highestElevation < Int(point.altitude) {
                 highestElevation = Int(point.altitude)
             }
+            clPoints.append([point.coordinate.latitude,point.coordinate.longitude])
         }
-        let run = ImportRun(time: time, date: "\(Date())", name: name, elevation: <#T##Int#>, route: <#T##[[Int]]#>)
-        
+        let totalElevationGain = startingElevation.distance(to: highestElevation)
+        let run = ImportRun(time: time, date: "\(Date())", name: name, elevation: totalElevationGain, route: clPoints)
+        //add it locally before the push up
+        let _ = Run(run: run, user: user)
+        pushRunToDataBase(run: run) { (success) in
+            completion(success)
+        }
+    }
+    
+    fileprivate func pushRunToDataBase(run: ImportRun, completion: @escaping (Bool) -> Void) {
+        guard var baseURL = URL(string: "https://runm8-a2d91.firebaseio.com/") else {completion(false);print("could not create url");return}
+        baseURL.appendPathComponent("users")
+        let apikey = retrieveValueFromPlist(key: "RunM8Key", plistName: "APIKeys")
+        let queryItem = URLQueryItem(name: "key", value: apikey)
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.queryItems = [queryItem]
+        guard var url = urlComponents?.url else {completion(false);print("url could not be pulled from urlcomponenets");return}
+        url.appendPathExtension("json")
+        var request = URLRequest(url: url)
+        var runData: Data?
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(run)
+            runData = data
+        } catch {
+            print("there was an error in \(#function) :\(error) : \(error.localizedDescription)")
+            completion(false)
+            return
+        }
+        guard let runDataUnwrapped = runData else {return}
+        request.httpBody = runDataUnwrapped
+        request.httpMethod = "PUT"
     }
     
     
